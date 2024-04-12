e01bfe5149c9b39739761f6756731d337252e915
Tittaya.W <97596208+Prince-in-the-wood@users.noreply.github.com>
2024-04-12 16:42:45 +0700

    Add http server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const socket = require('socket.io');
 
 const groups = new Set(); // keep the {isGroup : boolean , groupName : string}
@@ -16,9 +17,9 @@ class Connection {
         socket.on('join-group', (group) => this.joinGroup(group));
         socket.on('join-private', (username) => this.joinPrivate(username));
 
-        socket.on('send-group-message', (message, group,isText) => this.sendGroupMessage(message, group,isText));
-        socket.on('send-private-message',(message, username,isText) => this.sendPrivateMessage(message,username,isText));
-        
+        socket.on('send-group-message', (message, group, isText) => this.sendGroupMessage(message, group, isText));
+        socket.on('send-private-message', (message, username, isText) => this.sendPrivateMessage(message, username, isText));
+
         socket.on('disconnect', () => this.disconnect());
         socket.on('connect_error', (err) => {
             console.log(`connect_error due to ${err.message}`);
@@ -37,64 +38,64 @@ class Connection {
 
     joinGroup(group) {
         this.socket.join(group);
-        this.sendGroupMessage('joining', group,true);
+        this.sendGroupMessage('joining', group, true);
         const groupNameArray = Array.from(groups).map(g => g[1]);
         if (!groupNameArray.includes(group)) {
-            groups.add([true,group]);
+            groups.add([true, group]);
             this.io.sockets.emit('available', { "users": Array.from(users.values()), "groups": Array.from(groups) });
         }
     }
 
     joinPrivate(targetUsername) {
         const targetSocket = [...users.entries()].find(([socket, username]) => username === targetUsername)?.[0];
-        if(!targetSocket){
+        if (!targetSocket) {
             console.log(`Username ${targetUsername} not found`);
             return;
         }
         const myUsername = users.get(this.socket)
-        const privateChat = this.hashPrivateChatName(myUsername,targetUsername);
+        const privateChat = this.hashPrivateChatName(myUsername, targetUsername);
         this.socket.join(privateChat);
         targetSocket.join(privateChat);
-        this.sendPrivateMessage('joining',targetUsername,true)
+        this.sendPrivateMessage('joining', targetUsername, true)
         const groupNameArray = Array.from(groups).map(g => g[1]);
-        if(!groupNameArray.includes(privateChat)){
-            groups.add([false,privateChat]);
+        if (!groupNameArray.includes(privateChat)) {
+            groups.add([false, privateChat]);
             this.io.sockets.emit('available', { "users": Array.from(users.values()), "groups": Array.from(groups) });
         }
     }
 
-    sendGroupMessage(message, group , isText) {
+    sendGroupMessage(message, group, isText) {
         const myUsername = users.get(this.socket)
         const msg = {
             chatName: group,
             from: myUsername,
             message: message || "",
-            isText : isText,
+            isText: isText,
             time: (new Date()).toISOString()
         }
         this.io.sockets.to(group).emit("group-" + group, msg);
     }
-    
-    sendPrivateMessage(message,targetUsername,isText){
+
+    sendPrivateMessage(message, targetUsername, isText) {
         const targetSocket = [...users.entries()].find(([socket, username]) => username === targetUsername)?.[0];
-        if(!targetSocket){
+        if (!targetSocket) {
             console.log(`Username ${targetUsername} not found`);
             return;
         }
         const myUsername = users.get(this.socket);
-        const privateChat = this.hashPrivateChatName(myUsername,targetUsername);
+        const privateChat = this.hashPrivateChatName(myUsername, targetUsername);
         const msg = {
             chatName: privateChat,
             from: myUsername,
             message: message || "",
-            isText : isText,
+            isText: isText,
             time: (new Date()).toISOString()
         }
-        this.io.sockets.to(privateChat).emit("private-"+ privateChat,msg);
+        this.io.sockets.to(privateChat).emit("private-" + privateChat, msg);
     }
 
-    hashPrivateChatName(username1,username2){
-        if(username2 < username1) [username1,username2] = [username2,username1]
+    hashPrivateChatName(username1, username2) {
+        if (username2 < username1) [username1, username2] = [username2, username1]
         return `${username1}-${username2}`;
     }
 
@@ -110,8 +111,15 @@ function chat(io) {
     });
 }
 
+const PORT = process.env.PORT || 3000
+
+const server = http.createServer();
+server.listen(PORT, () => {
+    console.log(`Server running at PORT ${PORT}`);
+});
+
 const io = socket(
-    3000, {
+    server, {
     cors: {
         origin: "*",
         methods: ["GET", "POST"]
